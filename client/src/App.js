cf61ceb254b87245703098844a414117fb2f0ec3
Anastasia Mironova <anast.mir22@yandex.ru>
2020-11-18 14:15:03 +0300

    add: code and configure

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { BrowserRouter as Router } from "react-router-dom";
+import "materialize-css";
+import { useRouts } from "./routes";
+function App() {
+  const routes = useRouts(false);
+  return (
+    <Router>
+      {" "}
+      <div className="container">{routes}</div>
+    </Router>
+  );
+}
+
+export default App;
